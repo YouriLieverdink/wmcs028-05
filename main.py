822c8f92b5ecad20c00beeff318e2e2aae67e1ab
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2024-10-13 21:45:16 +0200

    feat: add graph

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import networkx as nx
+import numpy as np
 
-from networkx.algorithms import bipartite
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # Buid a graph from the first [n] edges defined at [path].
 def make(path, n):
     file = open(path, 'r')
-    graph = nx.Graph()
+    G = nx.Graph()
 
     for x in range(0, n):
         line = file.readline().split()
@@ -14,24 +16,32 @@ def make(path, n):
 
         # Split the line into the available columns 
         user, page, weight, timestamp = line
-        graph.add_edge(
-            f"u{user}", 
-            f"p{page}", 
-            weight=weight, 
-            timestamp=timestamp
-        )
+
+        G.add_node(f'u{user}', subset="user")
+        G.add_node(f'p{page}', subset="page")
+        G.add_edge(f'u{user}', f'p{page}', weight=weight, timestamp=timestamp)
 
     file.close()
 
-    return graph
-
+    return G
 
 def main():
-    graph = make('./assets/edges.txt', 100_000_000)
+    G = make('./assets/edges.txt', 500)
 
-    print(nx.number_of_nodes(graph))
-    print(nx.number_of_edges(graph))
+    # G.remove_nodes_from(list(nx.isolates(G)))
+    largest_cc = max(nx.connected_components(G), key=len)
 
+    sG = G.subgraph(largest_cc)
+
+    top = nx.bipartite.sets(sG)[0]
+    pos = nx.bipartite_layout(sG, top)
+
+    options = {
+        "node_size": 5,
+    }
+
+    nx.draw(sG, pos, **options)
+    plt.show()
 
 if __name__ == '__main__':
     main()
